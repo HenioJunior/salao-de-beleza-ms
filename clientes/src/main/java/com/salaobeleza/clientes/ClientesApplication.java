6879c96d18593dc933f527128b908f9de366d50f
Henio Junior <henioajunior@gmail.com>
2025-02-19 16:50:34 -0300

    Implementado o EurekaServer

--- clientes/src/main/java/com/salaobeleza/clientes/ClientesApplication.java
+++ clientes/src/main/java/com/salaobeleza/clientes/ClientesApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "API Salão de Beleza", version = "1", description = "API para cadastro de clientes"))
 public class ClientesApplication {
 
