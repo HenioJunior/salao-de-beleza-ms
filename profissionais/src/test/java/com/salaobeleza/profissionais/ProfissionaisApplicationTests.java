cb7211c3b1c0d457cbf9f5db2e2960ed9fdcc996
Henio Junior <henioajunior@gmail.com>
2025-02-21 11:44:47 -0300

    Implementado a API de cadastro de profissionais

---
+++ profissionais/src/test/java/com/salaobeleza/profissionais/ProfissionaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salaobeleza.profissionais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfissionaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
